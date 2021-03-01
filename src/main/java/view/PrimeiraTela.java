fb9304be56018bcca9a6f5241bc339475970fad3
vcpjr <vilmarcesarpereira@gmail.com>
2021-03-01 10:01:12 -0300

    Projeto maven e com primeira classe criada.

---
+++ src/main/java/view/PrimeiraTela.java
@@ -0,0 +1,46 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class PrimeiraTela extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PrimeiraTela frame = new PrimeiraTela();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PrimeiraTela() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		JLabel lblOla = new JLabel("Olá!");
+		contentPane.add(lblOla, BorderLayout.CENTER);
+	}
+
+}
