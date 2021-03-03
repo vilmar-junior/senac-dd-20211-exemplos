d87469233a3b9222863b71abbfe3882ba09dddc4
vcpjr <vilmarcesarpereira@gmail.com>
2021-03-03 08:24:46 -0300

    Alteracao na tela (maquina 1)

--- src/main/java/view/PrimeiraTela.java
+++ src/main/java/view/PrimeiraTela.java
@@ -39,7 +39,7 @@ public class PrimeiraTela extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 
-		JLabel lblOla = new JLabel("Olá!");
+		JLabel lblOla = new JLabel("Olá, alterei esse texto na máquina 1 (local)");
 		contentPane.add(lblOla, BorderLayout.CENTER);
 	}
 
